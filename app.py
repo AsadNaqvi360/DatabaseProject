4d8ae3b8894b9a9831aa9b7faefd3cdee8e659ea
KevinLAtUOIT <90418535+KevinLAtUOIT@users.noreply.github.com>
2025-04-02 19:43:42 -0400

    Update app.py
    New comment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Hi - Kevin
 from flask import Flask, render_template, request, redirect, url_for, flash
 import mysql.connector
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
