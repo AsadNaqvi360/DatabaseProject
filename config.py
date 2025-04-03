f1c4489f4f8c876ec24e28fad060280339a9ef76
AsadNaqvi360 <asad.naqvi@ontariotechu.net>
2025-04-02 22:02:37 -0400

    update

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
-import os
-
 DB_CONFIG = {
-    'host': 'Naqvi.mysql.pythonanywhere-services.com',  # ← CHANGED
-    'user': 'Naqvi',  # your PythonAnywhere username
+    'host': 'Naqvi.mysql.pythonanywhere-services.com',
+    'user': 'Naqvi',
     'password': 'INFRPROJECT123',
-    'database': 'Naqvi$infr'  # your full DB name with $
+    'database': 'Naqvi$infr'
 }
