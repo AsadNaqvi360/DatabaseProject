59b797f7c1b5c8cebc0e04ec3f4912db9e61cfea
AsadNaqvi360 <asad.naqvi@ontariotechu.net>
2025-04-02 22:07:02 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 DB_CONFIG = {
     'host': 'Naqvi.mysql.pythonanywhere-services.com',
     'user': 'Naqvi',
-    'password': 'INFRPROJECT123',
+    'password': 'Happy657063!',
     'database': 'Naqvi$infr'
 }
